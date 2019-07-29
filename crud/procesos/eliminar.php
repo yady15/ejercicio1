adb407da9b8b0d9a7e15268aa9e42921a71511c8
yady15 <yrprieto87@misena.edu.co>
2019-07-29 08:02:28 -0400

    primer commit

---
+++ crud/procesos/eliminar.php
@@ -0,0 +1,11 @@
+<?php
+$id=$_GET['id'];
+require_once "../conexion.php";
+require_once "../metodosCrud.php";
+$obj= new metodos();
+if($obj->eliminarDatosNombre($id)==1){
+header("location:../index.php");
+}else{
+echo "fallo al agregar";
+}
+?>
